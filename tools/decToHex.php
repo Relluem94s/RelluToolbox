cc8c4fab323bd3b070bc61ae141b3b6fa959fa96
Relluem94 <github@relluem94.de>
2023-07-08 14:23:26 +0200

    added dec to hex

---
+++ tools/decToHex.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_dec_string" oninput="$('#output_hex_string')[0].value = parseInt($('#original_dec_string')[0].value).toString(16).toUpperCase();">
+<input type="text" class="form-control" id="output_hex_string" disabled>
