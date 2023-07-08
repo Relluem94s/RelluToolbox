f23ceaf7145c1eeaa1860c30b98591fa8f834fcb
Relluem94 <github@relluem94.de>
2023-07-08 14:55:06 +0200

    converted noteshifter from php to js (wip)

--- tools/noteShifter.php
+++ tools/noteShifter.php
@@ -3,36 +3,11 @@
 
 $note = "";
 $shift = 0;
-$isNegative = false;
 $notes_show = array("C","C#","D","D#","E","F","F#","G","G#","A","A#","B");
-$notes = array("C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F","F#","G","G#","A","A#","B");
-
-if(isset($_POST['note']) && isset($_POST['shift'])){
-  $note = $_POST['note'];
-  $shift = $_POST['shift'];
-  
-  if($shift < 0){
-    $notes_show = array_reverse($notes_show);
-    $notes = array_reverse($notes);
-    $shift = abs($shift);
-    $isNegative = true;
-  }
-  
-  for($i = 0; $i < sizeof($notes); $i++){
-    if($notes[0] != $note){
-      $temp = array_shift($notes);
-      $notes[] = $temp;
-      
-      $temp = array_shift($notes_show);
-      $notes_show[] = $temp;
-    }
-  }
-}
-
-$note = $notes[$shift];
 
 
-$notes_select = '<select name="note">';
+
+$notes_select = '<select id="note_input" name="note">';
 
 for($i = 0; $i < sizeof($notes_show); $i++){
   $notes_select .= '<option>'.$notes_show[$i].'</option>';
@@ -40,15 +15,66 @@ for($i = 0; $i < sizeof($notes_show); $i++){
 
 $notes_select .= '</select>';
 
-if($isNegative == true){
-  $shift = -1 * abs($shift);
+
+
+
+$shift_select = '<input id="note_shift" name="shift" min="-23" max="23" type="number" value="'.$shift.'">';
+
+$shift_out = '<input type="text" id="note_output" disabled value="">';
+
+$shift_submit = '<input type="button" onclick="shift()" value="Berechne">';
+
+
+echo $notes_select . "&nbsp;" . $shift_select . "&nbsp;" . $shift_out . "&nbsp;" . $shift_submit;
+
+?>
+
+<script>
+
+function shift(){
+  
+
+var notes_show = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+var notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+
+var isNegative = false;
+
+var note = $('#note_input')[0].value;
+var shift = $('#note_shift')[0].value;
+  
+console.log(note);
+console.log(shift);
+
+  if(shift < 0){
+    notes_show = notes_show.reverse();
+    notes = notes.reverse();
+    shift = Math.abs(shift);
+    isNegative = true;
+  }
+  
+  for(var i = 0; i < notes.length; i++){
+    if(notes[0] != note){
+      var temp = notes.shift();
+      notes = temp;
+      
+      temp = notes_show.shift();
+      notes_show = temp;
+    }
+  }
+
+
+if(isNegative == true){
+  shift = -1 * Math.abs(shift); //TODO has to be fixed (negative undefined)
 }
 
-$shift_select = '<input name="shift" min="-23" max="23" type="number" value="'.$shift.'">';
 
-$shift_out = '<input type="text" disabled value="'.$note.'">';
-
-$shift_submit = '<input type="submit" value="Berechne">';
+$('#note_output')[0].value = notes[shift];
 
 
-echo '<form action="'.$_SERVER['REQUEST_URI'].'" method="post">' . $notes_select . "&nbsp;" . $shift_select . "&nbsp;" . $shift_out . "&nbsp;" . $shift_submit . '</form>';
+console.log($('#note_output')[0].value);
+console.log(shift);
+
+
+}
+
+</script>
