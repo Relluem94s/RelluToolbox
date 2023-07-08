f96fa29c0e84fd0efd17fa4fbcf173906e803b32
Relluem94 <github@relluem94.de>
2023-07-08 14:24:46 +0200

    added base64 converter

---
+++ tools/base64decode.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_encoded_string" oninput="$('#decoded_output_string')[0].value = atob($('#original_encoded_string')[0].value);">
+<input type="text" class="form-control" id="decoded_output_string" disabled>
---
+++ tools/base64encode.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_decoded_string" oninput="$('#encoded_output_string')[0].value = btoa($('#original_decoded_string')[0].value);">
+<input type="text" class="form-control" id="encoded_output_string" disabled>
