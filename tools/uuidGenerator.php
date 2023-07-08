440e6dc2c9efe4207a5052769f79882b1e51d90a
Relluem94 <github@relluem94.de>
2023-07-08 18:08:37 +0200

    added php 5 backport

--- tools/uuidGenerator.php
+++ tools/uuidGenerator.php
@@ -1,4 +1,19 @@
 <?php
+
+if( !function_exists('random_bytes') )
+{
+    function random_bytes($length = 6)
+    {
+        $characters = '0123456789';
+        $characters_length = strlen($characters);
+        $output = '';
+        for ($i = 0; $i < $length; $i++)
+            $output .= $characters[rand(0, $characters_length - 1)];
+
+        return $output;
+    }
+}
+
 function formatUUIDv4($data){
   assert(strlen($data) == 16);
 
