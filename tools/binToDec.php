bf9919f837634c1819107a454bce013218557f90
Relluem94 <github@relluem94.de>
2023-07-08 17:28:25 +0200

    added bin to dec and dec to bin

---
+++ tools/binToDec.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_bin_string" oninput="$('#output_dec2_string')[0].value = parseInt($('#original_bin_string')[0].value, 2);">
+<input type="text" class="form-control" id="output_dec2_string" disabled>
---
+++ tools/decToBin.php
@@ -0,0 +1,2 @@
+<input type="number" class="form-control" id="original_dec2_string" oninput="$('#output_bin_string')[0].value = parseInt($('#original_dec2_string')[0].value).toString(2);">
+<input type="text" class="form-control" id="output_bin_string" disabled>
