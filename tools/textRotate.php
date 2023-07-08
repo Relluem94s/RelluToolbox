5af2a338401582e8f727005348aa00164b308afa
Relluem94 <github@relluem94.de>
2023-07-08 13:19:30 +0200

    added text rotate

---
+++ tools/textRotate.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_string" oninput="$('#reverse_string')[0].value = $('#original_string')[0].value.split('').reverse().join('');">
+<input type="text" class="form-control" id="reverse_string" disabled>
