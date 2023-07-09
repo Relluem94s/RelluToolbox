b903681589544f8368dac6512d2248047ea488b2
Relluem94 <github@relluem94.de>
2023-07-09 14:21:28 +0200

    added text upper and lower case

---
+++ tools/textLowerCase.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_low_string" oninput="$('#lower_string')[0].value = $('#original_low_string')[0].value.toLowerCase();">
+<input type="text" class="form-control" id="lower_string" disabled>
---
+++ tools/textUpperCase.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control" id="original_up_string" oninput="$('#upper_string')[0].value = $('#original_up_string')[0].value.toUpperCase();">
+<input type="text" class="form-control" id="upper_string" disabled>
