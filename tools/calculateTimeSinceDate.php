c688143cc9caa0819c580147047e04067e557390
Relluem94 <github@relluem94.de>
2023-07-30 23:58:09 +0200

    RTB-1 fixed time mess (js math sucks)

--- tools/calculateTimeSinceDate.php
+++ tools/calculateTimeSinceDate.php
@@ -1,53 +1,60 @@
 <label for="inputDate">Enter a date (YYYY-MM-DD):</label>
+<div class="input-group">
   <input type="date" id="inputDate" class="form-control">
   <button onclick="calculateTimeSince()" class="form-control btn btn-success">Calculate</button>
-  <p id="resultDateDiff" class="mt-3"></p>
-  <p id="totalDays"></p>
+</div>
 
-  <script>
-    function calculateTimeSince() {
-      let inputDateValue = $("#inputDate").val();
+<p id="resultDateDiff" class="mt-3"></p>
+<p id="totalDays"></p>
 
-      if (!inputDateValue) {
-        $("#totalDays").text("Please enter a date.")
-        return;
-      }
+<script>
+  function calculateTimeSince() {
+    let inputDateValue = $("#inputDate").val();
 
-      let inputDate = new Date(inputDateValue);
-
-      let { years, months, days, totalDays, isFuture } = getTimeSince(inputDate);
-      
-      if (isFuture) {
-        $("#resultDateDiff").text("The date is in the future!");
-        $("#totalDays").text("");
-      } else {
-        $("#resultDateDiff").text(`Before ${years} years, ${months} months, and ${days} days`);
-        $("#totalDays").text(`Total days elapsed: ${totalDays}`);
-      }
+    if (!inputDateValue) {
+      $("#totalDays").text("Please enter a date.")
+      return;
     }
 
-    function getTimeSince(date) {
-      let now = new Date();
-      let timeDiff = now - date;
+    let inputDate = new Date(inputDateValue);
 
-      
-      let seconds = Math.floor(timeDiff / 1000);
-      let minutes = Math.floor(seconds / 60);
-      let hours = Math.floor(minutes / 60);
-      let days = Math.floor(hours / 24);
-      
-      if (timeDiff < 0) {
-        let isFuture = true;
-        return { years: 0, months: 0, days, totalDays: 0, isFuture };
-      }
-      
+    let { years, months, days, totalDays, isFuture } = getTimeSince(inputDate);
 
-      let years = Math.floor(days / 365);
-      let months = Math.floor((days / 365 * 12) - years * 12);
-      days = days - years * 365;
-      let totalDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-      let isFuture = false;
-
-      return { years, months, days: days - months * 30, totalDays, isFuture };
+    if (isFuture) {
+      $("#resultDateDiff").text("The date is in the future!");
+      $("#totalDays").text("");
+    } else {
+      $("#resultDateDiff").text(`Before ${years} years, ${months} months, and ${days} days`);
+      $("#totalDays").text(`Total days elapsed: ${totalDays}`);
     }
-  </script>
+  }
+
+  function getTimeSince(date) {
+    const DAYS_IN_YEAR = 365;
+    const MONTH_IN_YEAR = 12;
+    const HOUR_A_DAY = 24;
+    const MINUTES_A_HOUR = 60;
+    const SECONDS_A_MINUTE = 60;
+    const MILLISECONDS_A_SECOND = 1000;
+
+    let now = new Date();
+    let timeDiff = now - date;
+
+    if (timeDiff < 0) {
+      let isFuture = true;
+      return { years: 0, months: 0, days: 0, totalDays: 0, isFuture };
+    }
+
+    let seconds = (timeDiff / MILLISECONDS_A_SECOND);
+    let minutes = (seconds / SECONDS_A_MINUTE);
+    let hours = (minutes / MINUTES_A_HOUR);
+    let days = (hours / HOUR_A_DAY);
+    let years = Math.floor(days / DAYS_IN_YEAR);
+    let months = Math.floor(days / DAYS_IN_YEAR * MONTH_IN_YEAR - years * MONTH_IN_YEAR);
+    days = Math.ceil(days / DAYS_IN_YEAR / (days / DAYS_IN_YEAR * MONTH_IN_YEAR - years * MONTH_IN_YEAR));
+    let totalDays = Math.floor(timeDiff / (MILLISECONDS_A_SECOND * SECONDS_A_MINUTE * MINUTES_A_HOUR * HOUR_A_DAY));
+    let isFuture = false;
+
+    return { years, months, days, totalDays, isFuture };
+  }
+</script>
