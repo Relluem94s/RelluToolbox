b4df3055cdcb18a0eaa03d85e5f82b7fd810900a
Relluem94 <github@relluem94.de>
2023-08-04 18:13:52 +0200

    moved headline to displayname

--- tools/stockApp.php
+++ tools/stockApp.php
@@ -1,17 +1,15 @@
 <div class="container mt-5">
-    <h1 class="mb-4">Aktien Portfolio Tracker</h1>
-
     <form id="portfolioForm">
         <div class="form-group">
-            <label for="symbol">Aktien Symbol:</label>
+            <label for="symbol">Stock Symbol:</label>
             <input type="text" class="form-control" name="symbol" id="symbol" required>
         </div>
         <div class="form-group">
-            <label for="quantity">Die Menge:</label>
+            <label for="quantity">Share Count:</label>
             <input type="number" class="form-control" name="quantity" id="quantity" required>
         </div>
         <br>
-        <button type="button" class="btn btn-primary" id="addStockButton">Aktie Hinzufügen</button>
+        <button type="button" class="btn btn-primary" id="addStockButton">Add Stock</button>
     </form>
 
     <div id="portfolioList" class="mt-4">
@@ -48,8 +46,8 @@
                 portfolioList.appendChild(stockItem);
             })
             .catch(error => {
-                console.error('Fehler beim Abrufen der Aktiendaten:', error);
-                alert('Fehler beim Abrufen der Aktiendaten. Bitte versuchen Sie es später erneut.');
+                console.error(error);
+                alert('Error retrieving the Stock data. Please try again later.');
             });
     }
 </script>
