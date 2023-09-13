d1e608b0bd7dd14a592e10f7513bef6a2e502bd5
Relluem94 <github@relluem94.de>
2023-09-13 17:02:10 +0200

    added i in queryselector for ignoreCase & never nested resultlist lenght

--- assets/scripts/slothSearch.js
+++ assets/scripts/slothSearch.js
@@ -4,33 +4,34 @@
  * @param {*} event 
  */
 window.onload = (event) => {
-    
-    $("#searchBox").keyup(function() {
-        let searchTerm = ($(this).val());
-        
-        if(searchTerm == null || searchTerm == "" || searchTerm == " "){
-            $(".info-box").removeClass("hideItems");
-        }
-        else{
-            $(".info-box").addClass("hideItems");
-        }
-       
-        let resultList = document.querySelectorAll('.info-box[id*="'+searchTerm+'"]');
+  $("#searchBox").keyup(function () {
+    let searchTerm = ($(this).val().toLowerCase());
 
-        if(resultList.length > 0){
-          resultList.forEach(
-            function(node) {
-              let foundItemID = node.id;
-              let elem = document.getElementById(foundItemID);
+    if (searchTerm == null || searchTerm == "" || searchTerm == " ") {
+      $(".info-box").removeClass("hideItems");
+    }
+    else {
+      $(".info-box").addClass("hideItems");
+    }
 
-              if(elem.id.includes(searchTerm)){
-                elem.classList.remove('hideItems');
-              }
-              else{
-                $(".info-box").removeClass("hideItems");
-              }
-            }
-          );
+    let resultList = document.querySelectorAll('.info-box[id*="' + searchTerm + '" i]');
+
+    if (resultList.length < 0) {
+      return;
+    }
+
+    resultList.forEach(
+      function (node) {
+        let foundItemID = node.id;
+        let elem = document.getElementById(foundItemID);
+
+        if (elem.id.includes(searchTerm)) {
+          elem.classList.remove('hideItems');
         }
-      });
-  };
+        else {
+          $(".info-box").removeClass("hideItems");
+        }
+      }
+    );
+  });
+};
