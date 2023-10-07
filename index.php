501641db87d2e1e498e9023d56a8cb15bb3bc7bf
folivora97 <dirkzeller1@gmail.com>
2023-10-08 00:32:14 +0200

    Reworked CSS Stuff

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 function getTool(
     $name,
     $displayname,
@@ -7,7 +7,7 @@ function getTool(
     $description,
     $bgclass,
     $content
-    ) {
+) {
     return '
     <div class="col col-sm-4">
         <div style="cursor:pointer"
@@ -17,8 +17,8 @@ function getTool(
         data-bs-target="#' . $name . '">
             <span class="info-box-icon"><i class="' . $icon . '"></i></span>
             <div class="info-box-content">
-                  <span class="info-box-text">' . $displayname . '</span>
-                  <span class="info-box-number">' . $description . '</span>
+                  <span class="info-box-number">' . $displayname . '</span>
+                  <span class="info-box-text">' . $description . '</span>
               </div>
         </div>
         <div
@@ -52,64 +52,67 @@ function getTool(
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>RelluToolbox</title>
-        <script src="./node_modules/admin-lte/dist/js/adminlte.min.js"></script>
-        <script src="./node_modules/@fortawesome/fontawesome-free/js/all.min.js"></script>
-        <script src="./node_modules/jquery/dist/jquery.min.js"></script>
-        <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-        <script defer src="./assets/scripts/slothSearch.js"></script>
-        <link rel="stylesheet" href="./node_modules/admin-lte/dist/css/adminlte.min.css">
-        <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-        <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./assets/css/Toolbox.css">
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.png">
-    </head>
-    <body>
-        <div class="wrapper">
-            <!-- Preloader -->
-            <div class="preloader" style="display:none;">
-                <img class="animation__shake" src="assets/img/rellutoolbox.svg" alt="rellutoolbox" width="600">
-            </div>
-        
-            <div class="content-wrapper" style="min-height: 768px;">
-            
-                <div class="content">
-                    <div class="container">
-                        <div class="row">
-                            <h1 class="text-center mt-3 pt-3 mb-3 pb-3">
-                                <img alt="logo" src="assets/img/rellutoolbox.svg" width="60%">
-                            </h1>
-                        </div>
-                        <div class="row">
-                            <?php
-                                $tools = json_decode(file_get_contents("./tools.json"), true);
-                                
-                                foreach($tools as $key => $value){
-                                    echo getTool(
-                                        $value["name"],
-                                        $value["displayname"],
-                                        $value["icon"],
-                                        $value["description"],
-                                        $value["bgclass"],
-                                        $value["content"]
-                                    );
-                                }
 
-                            ?>
-                        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RelluToolbox</title>
+    <script src="./node_modules/admin-lte/dist/js/adminlte.min.js"></script>
+    <script src="./node_modules/@fortawesome/fontawesome-free/js/all.min.js"></script>
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script defer src="./assets/scripts/slothSearch.js"></script>
+    <link rel="stylesheet" href="./node_modules/admin-lte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/Toolbox.css">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png">
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Preloader -->
+        <div class="preloader" style="display:none;">
+            <img class="animation__shake" src="assets/img/rellutoolbox.svg" alt="rellutoolbox" width="600">
+        </div>
+
+        <div class="content-wrapper" style="min-height: 768px;">
+
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <h1 class="text-center mt-3 pt-3 mb-3 pb-3">
+                            <img alt="logo" src="assets/img/rellutoolbox.svg" width="60%">
+                        </h1>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $tools = json_decode(file_get_contents("./tools.json"), true);
+
+                        foreach ($tools as $key => $value) {
+                            echo getTool(
+                                $value["name"],
+                                $value["displayname"],
+                                $value["icon"],
+                                $value["description"],
+                                $value["bgclass"],
+                                $value["content"]
+                            );
+                        }
+
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
-        <footer class="bg-dark fixed-bottom searchBar">
-            <div class="container">
-                <div class="btn-group">
-                    <input id="searchBox" type="text" class="form-control empty" id="iconified" placeholder="&#xF002;"/>
-                </div>
+    </div>
+    <footer class="bg-dark fixed-bottom searchBar">
+        <div class="container">
+            <div class="btn-group">
+                <input id="searchBox" type="text" class="form-control empty" id="iconified" placeholder="&#xF002;" />
             </div>
-        </footer>
-    </body>
+        </div>
+    </footer>
+</body>
+
 </html>
