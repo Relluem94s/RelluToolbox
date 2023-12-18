46751e28d077e72d5a841fecb7d731f45c6cabd1
Relluem94 <github@relluem94.de>
2023-12-19 00:31:54 +0100

    RTB-24 added Image Base64 Conversion

---
+++ tools/base64ImageEncode.php
@@ -0,0 +1,21 @@
+<input class="form-control btn, btn-info" type="file" id="imageInput" />
+<textarea class="form-control" id="image_output"></textarea>
+
+<button class="btn btn-primary" onclick="copyToClipboard('image_output', 'Image')">
+    <i class="fa-solid fa-clipboard"></i>
+    Copy
+</button>
+ <script>
+   document.getElementById('imageInput').addEventListener('change', function (event) {
+   var file = event.target.files[0];
+   if (file) {
+     var reader = new FileReader();
+
+     reader.onload = function (readerEvent) {
+     var base64String = readerEvent.target.result;
+     $("#image_output")[0].value=base64String;
+   }
+   reader.readAsDataURL(file);
+   }
+ });
+ </script>
