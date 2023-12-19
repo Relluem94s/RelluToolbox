46996b3b42daef644c45db9b2a52076510453428
Relluem94 <github@relluem94.de>
2023-12-19 20:58:09 +0100

    RTB-27 added Note Generator

---
+++ tools/noteGenerator.php
@@ -0,0 +1,111 @@
+<?php
+
+
+$notes = array("C","C#","D","D#","E","F","F#","G","G#","A","A#","B");
+
+
+
+$notes_select = '<select class="form-control" id="note_select">';
+
+for($i = 0; $i < sizeof($notes); $i++){
+  $notes_select .= '<option>'.$notes[$i].'</option>';
+}
+
+$notes_select .= '</select>';
+
+
+
+
+echo '<div class="input-group">' . $notes_select .
+'<button class="btn btn-warning" onclick="playNote()"><i class="fa-solid fa-play"></i></button></div>';
+
+?>
+
+
+
+<script>
+
+    var ctxClass = window.audioContext ||window.AudioContext || window.AudioContext || window.webkitAudioContext
+    var ctx = new ctxClass();
+
+    var playNote = (function () {
+    
+        return function (finishedCallback) {
+            let frequency;
+            switch($("#note_select")[0].value) {
+            case "C":
+                frequency = 261.63;
+                break;
+            case "C#":
+                frequency = 277.18;
+                break;
+            case "D":
+                frequency = 293.66;
+                break;
+            case "D#":
+                frequency = 311.138;
+                break;
+            case "E":
+                frequency = 329.63;
+                break;
+            case "F":
+                frequency = 349.23;
+                break;
+            case "F#":
+                frequency = 369.99;
+                break;
+            case "G":
+                frequency = 392.00;
+                break;
+            case "G#":
+                frequency = 415.30;
+                break;
+            case "A":
+                frequency = 440.00;
+                break;
+            case "A#":
+                frequency = 466.16;
+                break;
+            case "B":
+                frequency = 493.88;
+                break;
+            default:
+                frequency = 523.25;
+            }
+
+
+            duration = 500;
+
+            type = 1
+
+            if (typeof finishedCallback != "function") {
+                finishedCallback = function () {};
+            }
+
+            var osc = ctx.createOscillator();
+            var gainNode = ctx.createGain();
+
+            osc.frequency.value = frequency;
+
+            gainNode.connect(ctx.destination);
+
+            gainNode.gain.value = 1;
+
+            osc.connect(gainNode);
+
+            osc.type = "sine";
+
+            osc.connect(ctx.destination);
+            if (osc.noteOn) osc.noteOn(0); // old browsers
+            if (osc.start) osc.start(); // new browsers
+
+            setTimeout(function () {
+                if (osc.noteOff) osc.noteOff(0); // old browsers
+                if (osc.stop) osc.stop(); // new browsers
+                finishedCallback();
+            }, duration);
+
+        };
+    })();
+
+    </script>
