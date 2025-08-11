e0103bd74f76d8fba4e680e756e3521bc923c9bd
Relluem94 <github@relluem94.de>
2025-08-11 21:59:40 +0200

    RTB-79 Format Stock app

--- tools/stockApp.php
+++ tools/stockApp.php
@@ -6,20 +6,21 @@
                 <div class="mb-2">
                     <br>
                     <form id="portfolioForm">
-                    <div class="form-group">
-                        <label for="symbol">Stock Symbol:</label>
-                        <input type="text" class="form-control" name="symbol" id="symbol" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="quantity">Share Count:</label>
-                        <input type="number" class="form-control" name="quantity" id="quantity" required>
-                    </div>
-                    <br>
-                    <button type="button" class="btn btn-primary" id="addStockButton" onclick="fetchStockData()">Add Stock</button>
-                </form>
+                        <div class="form-group">
+                            <label for="symbol">Stock Symbol:</label>
+                            <input type="text" class="form-control" name="symbol" id="symbol" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="quantity">Share Count:</label>
+                            <input type="number" class="form-control" name="quantity" id="quantity" required>
+                        </div>
+                        <br>
+                        <button type="button" class="btn btn-primary" id="addStockButton" onclick="fetchStockData()">Add
+                            Stock</button>
+                    </form>
 
-                <div id="portfolioList" class="mt-4">
-                </div>
+                    <div id="portfolioList" class="mt-4">
+                    </div>
                 </div>
             </fieldset>
         </div>
