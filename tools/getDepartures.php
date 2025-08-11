c04f7667f53c30ed78818c9bca1b1b66a1790a35
Relluem94 <github@relluem94.de>
2025-08-11 22:10:17 +0200

    RTB-82 Change Style of Public Transport Departures

--- tools/getDepartures.php
+++ tools/getDepartures.php
@@ -1,16 +1,33 @@
-<form id="departuresForm" action="javascript:setManualAddress();">
-    <div class="input-group">
-        <input id="manualAddress" class="form-control" type="text"></input>
-        <button title="Search Address" type="button" class="btn btn-primary" id="getAddress"><i class="fa-solid fa-search"></i> Search</button>
-        <button title="Use current Geo Location" type="button" class="btn btn-secondary" id="getLocation"><i class="fa-solid fa-location-crosshairs"></i></button>
-    </div>
-</form>
-<br>
+<div class="container">
+    <div class="row row-cols-1">
+        <div class="col">
+            <fieldset class="fieldset-card-small">
+                <legend class="fieldset-legend">Departures</legend>
+                <div class="mb-2">
+                    <br>
+                    <form id="departuresForm" action="javascript:setManualAddress();">
+                        <div class="input-group">
+                            <input id="manualAddress" class="form-control" type="text"></input>
+                            <button title="Search Address" type="button" class="btn btn-primary" id="getAddress"><i
+                                    class="fa-solid fa-search"></i> Search</button>
+                            <button title="Use current Geo Location" type="button" class="btn btn-secondary"
+                                id="getLocation"><i class="fa-solid fa-location-crosshairs"></i></button>
+                        </div>
+                    </form>
+                    <br>
 
-<div id="availableStations">
+                    <div id="availableStations">
+                    </div>
+
+                    <div>This is using <a target="_blank"
+                            href="https://v6.db.transport.rest/api.html">v6.db.transport.rest</a> and <a target="_blank"
+                            href="https://openstreetmap.org">OpenStreetMap</a> Data </div>
+
+                </div>
+            </fieldset>
+        </div>
+    </div>
 </div>
 
-<div>This is using <a target="_blank" href="https://v6.db.transport.rest/api.html">v6.db.transport.rest</a> and <a target="_blank" href="https://openstreetmap.org">OpenStreetMap</a> Data </div>
-
-
 <script src="../shared/assets/js/departures.js">
+  
