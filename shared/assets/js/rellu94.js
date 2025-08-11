712e88326f19a502c41caf15609002080314d935
Relluem94 <github@relluem94.de>
2025-08-11 21:46:17 +0200

    RTB-78 Change Style of Get Weather

--- shared/assets/js/rellu94.js
+++ shared/assets/js/rellu94.js
@@ -385,3 +385,39 @@ function fetchStockData() {
     });
 }
 
+
+function fetchWeatherData() {
+    const city = document.getElementById('city').value;
+    fetch(`tools//weatherApp/getWeather.php?city=${city}`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('weatherInfo').innerHTML = "<br><br>" + data;
+            updateTemperatureColor();
+        })
+        .catch(error => {
+            console.error('Fehler beim Abrufen der Wetterdaten:', error);
+        });
+}
+
+function updateTemperatureColor() {
+    const temperatureElement = document.querySelector('p.temperature');
+    temperatureElement.style.fontWeight = "750";
+
+    const temperatureText = temperatureElement.textContent;
+
+    const normalizedTemperatureText = temperatureText.replace(/[^\d.,-]/g, '').replace(',', '.');
+
+    const temperature = parseFloat(normalizedTemperatureText);
+
+    if (temperature < 0) {
+        temperatureElement.style.color = 'turquoise';
+    } else if (temperature < 10) {
+        temperatureElement.style.color = 'blue';
+    } else if (temperature < 20) {
+        temperatureElement.style.color = 'orange';
+    }else {
+        temperatureElement.style.color = 'red';
+    }
+}
+
+
--- tools/getWeather.php
+++ tools/getWeather.php
@@ -1,12 +1,27 @@
-<form id="weatherForm">
-    <div class="input-group">
-        <input type="text" class="form-control" name="city" id="city" placeholder="Stadt Eingeben" required>
-        <button type="button" class="btn btn-primary" id="getWeatherButton">Wetter</button>
-    </div>
-</form>
+<div class="container">
+    <div class="row row-cols-1">
+        <div class="col">
+            <fieldset class="fieldset-card-small">
+                <legend class="fieldset-legend">Weather of City</legend>
+                <div class="mb-2">
+                    <br>
+                    <form id="weatherForm">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="city" id="city" placeholder="Enter the City" required>
+                        <button type="button" class="btn btn-primary" id="getWeatherButton">Wetter</button>
+                    </div>
+                </form>
 
-<div id="weatherInfo">
+                <div id="weatherInfo">
+                </div>
+                </div>
+            </fieldset>
+        </div>
+    </div>
 </div>
+
+
+
 
 <script>
     document.getElementById('getWeatherButton').addEventListener('click', function() {
@@ -18,38 +33,4 @@
         event.preventDefault();
         fetchWeatherData();
     });
-
-function fetchWeatherData() {
-    const city = document.getElementById('city').value;
-    fetch(`tools//weatherApp/getWeather.php?city=${city}`)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('weatherInfo').innerHTML = "<br><br>" + data;
-            updateTemperatureColor();
-        })
-        .catch(error => {
-            console.error('Fehler beim Abrufen der Wetterdaten:', error);
-        });
-}
-
-function updateTemperatureColor() {
-    const temperatureElement = document.querySelector('p.temperature');
-    temperatureElement.style.fontWeight = "750";
-
-    const temperatureText = temperatureElement.textContent;
-
-    const normalizedTemperatureText = temperatureText.replace(/[^\d.,-]/g, '').replace(',', '.');
-
-    const temperature = parseFloat(normalizedTemperatureText);
-
-    if (temperature < 0) {
-        temperatureElement.style.color = 'turquoise';
-    } else if (temperature < 10) {
-        temperatureElement.style.color = 'blue';
-    } else if (temperature < 20) {
-        temperatureElement.style.color = 'orange';
-    }else {
-        temperatureElement.style.color = 'red';
-    }
-}
 </script>
