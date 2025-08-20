6c52a7d3c8dcf66a1202b846774c17bfcffc6580
Relluem94 <github@relluem94.de>
2025-08-21 00:13:26 +0200

    RTB-93 Enlarge JSON Conversion Modal

--- tools/jsonConversion.php
+++ tools/jsonConversion.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="row row-cols-1">
+    <div class="row row-cols-1 row-cols-md-2 g-3">
 
         <div class="col">
             <fieldset class="fieldset-card-small">
